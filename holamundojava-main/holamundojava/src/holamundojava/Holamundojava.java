b1b1821729c83ad7e5bf74fcd91f3cbcca015272
IsraelMaganaSolis <israelmaganasolis@gmail.com>
2022-02-13 14:41:50 -0700

    Se agrega el proyecto

---
+++ holamundojava-main/holamundojava/src/holamundojava/Holamundojava.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundojava;
+
+/**
+ *
+ * @author Carlos Lázaro <carloslazaroam@gmail.com>
+ */
+public class Holamundojava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HolaMundo");
+        // TODO code application logic here
+    }
+    
+}
